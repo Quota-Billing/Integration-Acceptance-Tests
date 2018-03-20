5427d928e4ed0b1d9217702aef564e6348c5a6fe
Jack Hallam <hallamja@rose-hulman.edu>
2018-03-20 10:04:36 -0400

    Test duplicate sign up

--- src/test/java/SignUpLogInLogOutTest.java
+++ src/test/java/SignUpLogInLogOutTest.java
@@ -19,6 +19,20 @@ public class SignUpLogInLogOutTest {
   }
 
   @Test
+  public void testDuplicateSignUp() throws Exception {
+    UUID userRandom = UUID.randomUUID();
+    HttpResponse response1 = Unirest.post("http://localhost:8084/signUp").field("name", "testSignUp" + userRandom).field("password", "testSignUpPass").asString();
+    assertEquals(200, response1.getStatus());
+    HttpResponse response2 = Unirest.get("http://localhost:8084/dashboard").asString();
+    assertEquals(200, response2.getStatus());
+    assertFalse(response2.getBody().toString().isEmpty());
+    HttpResponse response3 = Unirest.get("http://localhost:8084/logout").asString();
+    assertEquals(200, response3.getStatus());
+    HttpResponse response4 = Unirest.post("http://localhost:8084/signUp").field("name", "testSignUp" + userRandom).field("password", "testSignUpPass").asString();
+    assertEquals(401, response4.getStatus());
+  }
+
+  @Test
   public void testLogInLogOut() throws Exception {
     UUID userRandom = UUID.randomUUID();
     HttpResponse response1 = Unirest.post("http://localhost:8084/signUp").field("name", "testLogIn" + userRandom).field("password", "testLogInPass").asString();
