d4870f798e1bb99663b11e022217b2b706103f0d
Jack Hallam <hallamja@rose-hulman.edu>
2018-03-20 09:58:02 -0400

    workaround database duplicate user

--- src/test/java/SignUpLogInLogOutTest.java
+++ src/test/java/SignUpLogInLogOutTest.java
@@ -2,13 +2,16 @@ import com.mashape.unirest.http.HttpResponse;
 import com.mashape.unirest.http.Unirest;
 import org.junit.Test;
 
+import java.util.UUID;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 
 public class SignUpLogInLogOutTest {
   @Test
   public void testSignUp() throws Exception {
-    HttpResponse response1 = Unirest.post("http://localhost:8084/signUp").field("name","testSignUp").field("password", "testSignUpPass").asString();
+    UUID userRandom = UUID.randomUUID();
+    HttpResponse response1 = Unirest.post("http://localhost:8084/signUp").field("name", "testSignUp" + userRandom).field("password", "testSignUpPass").asString();
     assertEquals(200, response1.getStatus());
     HttpResponse response2 = Unirest.get("http://localhost:8084/dashboard").asString();
     assertEquals(200, response2.getStatus());
@@ -17,13 +20,14 @@ public class SignUpLogInLogOutTest {
 
   @Test
   public void testLogInLogOut() throws Exception {
-    HttpResponse response1 = Unirest.post("http://localhost:8084/signUp").field("name","testLogIn").field("password", "testLogInPass").asString();
+    UUID userRandom = UUID.randomUUID();
+    HttpResponse response1 = Unirest.post("http://localhost:8084/signUp").field("name", "testLogIn" + userRandom).field("password", "testLogInPass").asString();
     assertEquals(200, response1.getStatus());
     HttpResponse response2 = Unirest.get("http://localhost:8084/logout").asString();
     assertEquals(200, response2.getStatus());
     HttpResponse response3 = Unirest.get("http://localhost:8084/dashboard").asString();
     assertEquals(403, response3.getStatus());
-    HttpResponse response4 = Unirest.post("http://localhost:8084/logIn").field("name","testLogIn").field("password", "testLogInPass").asString();
+    HttpResponse response4 = Unirest.post("http://localhost:8084/logIn").field("name", "testLogIn" + userRandom).field("password", "testLogInPass").asString();
     assertEquals(200, response4.getStatus());
     HttpResponse response5 = Unirest.get("http://localhost:8084/dashboard").asString();
     assertEquals(200, response5.getStatus());
